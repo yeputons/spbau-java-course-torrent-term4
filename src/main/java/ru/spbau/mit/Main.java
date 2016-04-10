cb5b176d6776ba833783a667bf47caa61f83be60
Egor Suvorov <egor.suvorov@gmail.com>
2016-04-10 22:01:05 +0300

    Initial commit: pom.xml and stubs

---
+++ src/main/java/ru/spbau/mit/Main.java
@@ -0,0 +1,10 @@
+package ru.spbau.mit;
+
+public final class Main {
+    private Main() {
+    }
+
+    public static void main(String[] args) {
+        throw new RuntimeException();
+    }
+}
---
+++ src/test/java/ru/spbau/mit/StubTest.java
@@ -0,0 +1,14 @@
+package ru.spbau.mit;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class StubTest {
+    @Test
+    public void testSettings() {
+        // CHECKSTYLE.OFF: MagicNumber
+        assertEquals(4, 2 * 2);
+        // CHECKSTYLE.ON: MagicNumber
+    }
+}
