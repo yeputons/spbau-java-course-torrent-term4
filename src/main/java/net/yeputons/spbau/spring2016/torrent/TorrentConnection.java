bcb4016f0e75f8068eabd36628b45578447c017e
Egor Suvorov <egor.suvorov@gmail.com>
2016-04-11 21:25:10 +0300

    Implement wrapping TorrentConnection class

---
+++ src/main/java/net/yeputons/spbau/spring2016/torrent/TorrentConnection.java
@@ -0,0 +1,50 @@
+package net.yeputons.spbau.spring2016.torrent;
+
+import net.yeputons.spbau.spring2016.torrent.protocol.Request;
+
+import java.io.Closeable;
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.InetSocketAddress;
+import java.net.Socket;
+
+public class TorrentConnection implements Closeable {
+    private final Socket socket;
+    private final DataInputStream in;
+    private final DataOutputStream out;
+
+    public TorrentConnection(InetSocketAddress address) throws IOException {
+        socket = new Socket(address.getAddress(), address.getPort());
+        try {
+            in = new DataInputStream(socket.getInputStream());
+            out = new DataOutputStream(socket.getOutputStream());
+        } catch (IOException e) {
+            try {
+                socket.close();
+            } catch (IOException ignored) {
+            }
+            throw e;
+        }
+    }
+
+    public <T> T makeRequest(Request<T> r) throws IOException {
+        return r.makeRequest(in, out);
+    }
+
+    @Override
+    public void close() {
+        try {
+            in.close();
+        } catch (IOException ignored) {
+        }
+        try {
+            out.close();
+        } catch (IOException ignored) {
+        }
+        try {
+            socket.close();
+        } catch (IOException ignored) {
+        }
+    }
+}
