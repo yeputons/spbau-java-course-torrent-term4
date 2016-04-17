279bbaa0f2e222d9ee688b12ef29fd1f555b9512
Egor Suvorov <egor.suvorov@gmail.com>
2016-04-17 23:38:23 +0300

    TorrentClient: extract TorrentLeecher class

--- src/main/java/net/yeputons/spbau/spring2016/torrent/client/TorrentClient.java
+++ src/main/java/net/yeputons/spbau/spring2016/torrent/client/TorrentClient.java
@@ -2,12 +2,8 @@ package net.yeputons.spbau.spring2016.torrent.client;
 
 import net.yeputons.spbau.spring2016.torrent.FileDescription;
 import net.yeputons.spbau.spring2016.torrent.TorrentConnection;
-import net.yeputons.spbau.spring2016.torrent.protocol.*;
 
 import java.io.*;
-import java.net.InetSocketAddress;
-import java.nio.ByteBuffer;
-import java.util.*;
 
 public class TorrentClient implements Runnable {
     private final TorrentConnection tracker;
@@ -28,55 +24,13 @@ public class TorrentClient implements Runnable {
         }
 
         for (FileDescription f : state.getFiles().values()) {
-            new Thread(() -> {
-                try {
-                    downloadFile(f);
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-            }).start();
+            new TorrentLeecher(tracker, state, f).start();
         }
 
         try {
             seeder.start();
         } catch (IOException e) {
             e.printStackTrace();
-        }
-    }
-
-    private void downloadFile(FileDescription description) throws IOException {
-        FileEntry entry = description.getEntry();
-        int fileId = entry.getId();
-        BitSet downloaded = description.getDownloaded();
-        int partsCount = description.getPartsCount();
-
-        while (downloaded.cardinality() < partsCount) {
-            List<InetSocketAddress> sources = tracker.makeRequest(new SourcesRequest(fileId));
-            for (InetSocketAddress source : sources) {
-                try (TorrentConnection peer = new TorrentConnection(source)) {
-                    List<Integer> partsAvailable = peer.makeRequest(new StatRequest(fileId));
-                    for (int partId : partsAvailable) {
-                        if (!downloaded.get(partId)) {
-                            ByteBuffer data = peer.makeRequest(
-                                    new GetRequest(fileId, partId, description.getPartSize(partId)));
-                            RandomAccessFile file = state.getFile(fileId);
-                            synchronized (file) {
-                                file.seek(description.getPartStart(partId));
-                                file.write(data.array());
-                            }
-                            downloaded.flip(partId);
-                        }
-                    }
-                } catch (IOException ignored) {
-                }
-            }
-            try {
-                // CHECKSTYLE.OFF: MagicNumber
-                Thread.sleep(1000);
-                // CHECKSTYLE.ON: MagicNumber
-            } catch (InterruptedException e) {
-                break;
-            }
         }
     }
 }
---
+++ src/main/java/net/yeputons/spbau/spring2016/torrent/client/TorrentLeecher.java
@@ -0,0 +1,73 @@
+package net.yeputons.spbau.spring2016.torrent.client;
+
+import net.yeputons.spbau.spring2016.torrent.FileDescription;
+import net.yeputons.spbau.spring2016.torrent.TorrentConnection;
+import net.yeputons.spbau.spring2016.torrent.protocol.FileEntry;
+import net.yeputons.spbau.spring2016.torrent.protocol.GetRequest;
+import net.yeputons.spbau.spring2016.torrent.protocol.SourcesRequest;
+import net.yeputons.spbau.spring2016.torrent.protocol.StatRequest;
+
+import java.io.IOException;
+import java.io.RandomAccessFile;
+import java.net.InetSocketAddress;
+import java.nio.ByteBuffer;
+import java.util.BitSet;
+import java.util.List;
+
+public class TorrentLeecher {
+    private final TorrentConnection tracker;
+    private final ClientState state;
+    private final FileDescription fileDescription;
+
+    public TorrentLeecher(TorrentConnection tracker, ClientState state, FileDescription fileDescription) {
+        this.tracker = tracker;
+        this.state = state;
+        this.fileDescription = fileDescription;
+    }
+
+    public void start() {
+        new Thread(() -> {
+            try {
+                downloadFile();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }).start();
+    }
+
+    private void downloadFile() throws IOException {
+        FileEntry entry = fileDescription.getEntry();
+        int fileId = entry.getId();
+        BitSet downloaded = fileDescription.getDownloaded();
+        int partsCount = fileDescription.getPartsCount();
+
+        while (downloaded.cardinality() < partsCount) {
+            List<InetSocketAddress> sources = tracker.makeRequest(new SourcesRequest(fileId));
+            for (InetSocketAddress source : sources) {
+                try (TorrentConnection peer = new TorrentConnection(source)) {
+                    List<Integer> partsAvailable = peer.makeRequest(new StatRequest(fileId));
+                    for (int partId : partsAvailable) {
+                        if (!downloaded.get(partId)) {
+                            ByteBuffer data = peer.makeRequest(
+                                    new GetRequest(fileId, partId, fileDescription.getPartSize(partId)));
+                            RandomAccessFile file = state.getFile(fileId);
+                            synchronized (file) {
+                                file.seek(fileDescription.getPartStart(partId));
+                                file.write(data.array());
+                            }
+                            downloaded.flip(partId);
+                        }
+                    }
+                } catch (IOException ignored) {
+                }
+            }
+            try {
+                // CHECKSTYLE.OFF: MagicNumber
+                Thread.sleep(1000);
+                // CHECKSTYLE.ON: MagicNumber
+            } catch (InterruptedException e) {
+                break;
+            }
+        }
+    }
+}
