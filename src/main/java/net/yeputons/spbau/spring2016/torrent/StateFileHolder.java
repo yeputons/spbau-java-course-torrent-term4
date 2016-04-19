0f2b4559a7167e56d7d3ede4ba5834bd63c2c7b4
Egor Suvorov <egor.suvorov@gmail.com>
2016-04-19 12:18:30 +0300

    StateFileHolder: add log loading process

--- src/main/java/net/yeputons/spbau/spring2016/torrent/StateFileHolder.java
+++ src/main/java/net/yeputons/spbau/spring2016/torrent/StateFileHolder.java
@@ -1,11 +1,15 @@
 package net.yeputons.spbau.spring2016.torrent;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import java.io.*;
 import java.nio.file.Files;
 import java.nio.file.NoSuchFileException;
 import java.nio.file.Path;
 
 public class StateFileHolder<T extends Serializable> implements StateHolder<T> {
+    private static final Logger LOG = LoggerFactory.getLogger(StateFileHolder.class);
     private final Path storage;
     private final T state;
 
@@ -13,11 +17,13 @@ public class StateFileHolder<T extends Serializable> implements StateHolder<T> {
     public StateFileHolder(Path storage, T defaultValue) {
         T state = defaultValue;
         this.storage = storage;
+        LOG.debug("Loading state of type {} from {}", defaultValue.getClass().getCanonicalName(), storage);
         try (ObjectInputStream in = new ObjectInputStream(Files.newInputStream(storage))) {
             state = (T) in.readObject();
         } catch (NoSuchFileException | FileNotFoundException ignored) {
+            LOG.debug("File not found, backing to default");
         } catch (IOException | ClassNotFoundException e) {
-            e.printStackTrace();
+            LOG.warn("Unable to load state from file, backing to default", e);
         }
         this.state = state;
     }
