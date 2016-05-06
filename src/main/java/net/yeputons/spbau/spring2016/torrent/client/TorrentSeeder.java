0a1eb53f88a6a11135bf237a1fe2ee3bcd25d6af
Egor Suvorov <egor.suvorov@gmail.com>
2016-05-07 01:38:07 +0300

    Add names to threads: seeder-listen, tracker-listen

--- src/main/java/net/yeputons/spbau/spring2016/torrent/client/TorrentSeeder.java
+++ src/main/java/net/yeputons/spbau/spring2016/torrent/client/TorrentSeeder.java
@@ -90,7 +90,7 @@ public class TorrentSeeder {
                     LOG.info("Client disconnected");
                 });
             }
-        });
+        }, "seeder-listen");
         listeningThread.start();
     }
 
