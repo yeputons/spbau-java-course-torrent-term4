a729214ad4af811f109f9bc93e63c602944e8243
Egor Suvorov <egor.suvorov@gmail.com>
2016-05-07 02:31:12 +0300

    test.sh: add regular server restarting to test client reconnection ability

--- test.sh
+++ test.sh
@@ -19,15 +19,17 @@ cd "$PROJ"
 mvn assembly:assembly 
 
 JAR=$PROJ/target/torrent-1.0-SNAPSHOT-jar-with-dependencies.jar
-SERVER="\"$JAVA\" -Dtorrent.part_size=102400 -cp \"$JAR\" ru.spbau.mit.TorrentTrackerMain"
-CLIENT="\"$JAVA\" -Dtorrent.part_size=102400 -cp \"$JAR\" ru.spbau.mit.TorrentClientMain"
+SERVER="\"$JAVA\" -Dtorrent.part_size=102400 -Dtorrent.update_interval=100 -Dtorrent.retry_delay=100 -cp \"$JAR\" ru.spbau.mit.TorrentTrackerMain"
+CLIENT="\"$JAVA\" -Dtorrent.part_size=102400 -Dtorrent.update_interval=100 -Dtorrent.retry_delay=100 -cp \"$JAR\" ru.spbau.mit.TorrentClientMain"
 
+echo -e "\e[1m========== STARTING SERVER ==========\e[0m"
 mkdir "$TMP/server"
 cd "$TMP/server"
 eval "$SERVER &"
-PIDS=$!
+SERVPID=$!
 
 for ((i=1; $i <= $CLIENTS; i++)); do
+  echo -e "\e[1m========== CONFIGURING CLIENT $i ==========\e[0m"
   mkdir "$TMP/client$i"
   cd "$TMP/client$i"
   if [ $i == 1 ]; then 
@@ -35,14 +37,28 @@ for ((i=1; $i <= $CLIENTS; i++)); do
   else
     eval $CLIENT get 127.0.0.1 1
   fi
+done
+for ((i=1; $i <= $CLIENTS; i++)); do
+  echo -e "\e[1m========== STARTING CLIENT $i ==========\e[0m"
+  cd "$TMP/client$i"
   eval "$CLIENT run 127.0.0.1 &"
   PIDS="$PIDS $!"
 done
 
-sleep 10
+echo -e "\e[1m========== WORKING ==========\e[0m"
+for ((step=1; $step <= 5; step++)); do
+  sleep 2
+  echo -e "\e[33;1m========== RESTARTING SERVER ==========\e[0m"
+  kill $SERVPID >/dev/null 2>&1 || true
+  kill -9 $SERVPID >/dev/null 2>&1 || true
+  cd "$TMP/server"
+  eval "$SERVER &"
+  SERVPID=$!
+done
 
-kill $PIDS >/dev/null 2>&1 || true
-kill -9 $PIDS >/dev/null 2>&1 || true
+echo -e "\e[1m========== KILLING ALL ==========\e[0m"
+kill $SERVPID $PIDS >/dev/null 2>&1 || true
+kill -9 $SERVPID $PIDS >/dev/null 2>&1 || true
 
 fail=0
 for ((i=1; $i <= $CLIENTS; i++)); do
@@ -51,8 +67,8 @@ for ((i=1; $i <= $CLIENTS; i++)); do
     fail=1
   fi
 done
+
 if [ $fail == 0 ]; then
   echo -e "\e[32;1mSUCCESS\e[0m"
 fi
-
 rm -rf $TMP || true
